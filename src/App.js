baa23120e3ccc085577df50bcecba4785abb490e
Lucamariano166 <lucamariano.lm166@gmail.com>
2024-09-10 17:39:05 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+
 
 function App() {
   return (
@@ -14,10 +14,10 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Skills />
       <Projects />
-      <Contact />
-      <footer />
+      <Skills />
+
+
     </div>
   );
 }
